c227f782f9fc9de577d15d6ec78c1a4c4fb288db
Jiwon Yoon <s2017i33@e-mirim.hs.kr>
2019-06-14 14:28:08 +0900

    초기 커밋.

---
+++ ProgressBar/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbStatus = new System.Windows.Forms.ProgressBar();
+            this.butbtnRunton1 = new System.Windows.Forms.Button();
+            this.btnENd = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.Location = new System.Drawing.Point(13, 13);
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(304, 52);
+            this.pbStatus.TabIndex = 0;
+            // 
+            // butbtnRunton1
+            // 
+            this.butbtnRunton1.Location = new System.Drawing.Point(27, 90);
+            this.butbtnRunton1.Name = "butbtnRunton1";
+            this.butbtnRunton1.Size = new System.Drawing.Size(75, 23);
+            this.butbtnRunton1.TabIndex = 1;
+            this.butbtnRunton1.Text = "실행";
+            this.butbtnRunton1.UseVisualStyleBackColor = true;
+            // 
+            // btnENd
+            // 
+            this.btnENd.Location = new System.Drawing.Point(123, 90);
+            this.btnENd.Name = "btnENd";
+            this.btnENd.Size = new System.Drawing.Size(75, 23);
+            this.btnENd.TabIndex = 2;
+            this.btnENd.Text = "멈춤";
+            this.btnENd.UseVisualStyleBackColor = true;
+            this.btnENd.Click += new System.EventHandler(this.BtnENd_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(219, 90);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "리셋";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(25, 135);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(33, 12);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "상태:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 155);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnENd);
+            this.Controls.Add(this.butbtnRunton1);
+            this.Controls.Add(this.pbStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "상태진행";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pbStatus;
+        private System.Windows.Forms.Button butbtnRunton1;
+        private System.Windows.Forms.Button btnENd;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
